a7cae50e9e0f386e7187b4c39ec1965337c27239
Phat Tran <prosandaru@gmail.com>
2020-04-25 21:29:20 +0700

    Packages update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
+
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'test',
+            entities: [],
+            synchronize: true,
+        }),
+    ],
+})
+
+@Module({
+    imports: [ConfigModule.forRoot()],
+})
+
 export class AppModule {}
